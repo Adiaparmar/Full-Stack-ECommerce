d68ac36821ac2fe888d55b023eefe5b75e2e8635
adiaparmar <adiaparmar@gmail.com>
2024-10-11 19:02:01 +0530

    Sidebar Complete

--- admin/src/components/Sidebar/index.js
+++ admin/src/components/Sidebar/index.js
@@ -5,15 +5,28 @@ import { FaProductHunt } from "react-icons/fa";
 import { FaCartArrowDown } from "react-icons/fa";
 import { IoIosSettings } from "react-icons/io";
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import { IoMdLogOut } from "react-icons/io";
 
 const Sidebar = () => {
+  const [activeTab, setActiveTab] = useState(null);
+  const [isToggleSubmenu, setIsToggleSubmenu] = useState(false);
+  const isOpenSubmenu = (index) => {
+    setActiveTab(index);
+    console.log(activeTab);
+    console.log(isToggleSubmenu);
+    setIsToggleSubmenu(!isToggleSubmenu);
+  };
   return (
     <>
       <div className="sidebar">
         <ul>
           <li>
             <Link to="/">
-              <Button className="w-100">
+              <Button
+                className={`w-100 ${activeTab === 0 ? "active" : ""}`}
+                onClick={() => isOpenSubmenu(0)}
+              >
                 <span className="icon">
                   <MdDashboard />
                 </span>
@@ -25,21 +38,46 @@ const Sidebar = () => {
             </Link>
           </li>
           <li>
-            <Link to="/">
-              <Button className="w-100">
-                <span className="icon">
-                  <FaProductHunt />
-                </span>
-                Products
-                <span className="arrow">
-                  <FaAngleRight />
-                </span>
-              </Button>
-            </Link>
+            <Button
+              className={`w-100 ${
+                activeTab === 1 && isToggleSubmenu === true ? "active" : ""
+              }`}
+              onClick={() => isOpenSubmenu(1)}
+            >
+              <span className="icon">
+                <FaProductHunt />
+              </span>
+              Products
+              <span className="arrow">
+                <FaAngleRight />
+              </span>
+            </Button>
+            <div
+              className={`submenuWrapper ${
+                activeTab === 1 && isToggleSubmenu === true
+                  ? "colapse"
+                  : "colapsed"
+              }`}
+            >
+              <ul className="submenu">
+                <li>
+                  <Link to="#">Product List</Link>
+                </li>
+                <li>
+                  <Link to="#">Product View</Link>
+                </li>
+                <li>
+                  <Link to="#">Product Upload</Link>
+                </li>
+              </ul>
+            </div>
           </li>
           <li>
             <Link to="/">
-              <Button className="w-100">
+              <Button
+                className={`w-100 ${activeTab === 2 ? "active" : ""}`}
+                onClick={() => isOpenSubmenu(2)}
+              >
                 <span className="icon">
                   <FaCartArrowDown />
                 </span>
@@ -52,7 +90,10 @@ const Sidebar = () => {
           </li>
           <li>
             <Link to="/">
-              <Button className="w-100">
+              <Button
+                className={`w-100 ${activeTab === 3 ? "active" : ""}`}
+                onClick={() => isOpenSubmenu(3)}
+              >
                 <span className="icon">
                   <MdMessage />
                 </span>
@@ -65,7 +106,10 @@ const Sidebar = () => {
           </li>
           <li>
             <Link to="/">
-              <Button className="w-100">
+              <Button
+                className={`w-100 ${activeTab === 4 ? "active" : ""}`}
+                onClick={() => isOpenSubmenu(4)}
+              >
                 <span className="icon">
                   <FaBell />
                 </span>
@@ -78,7 +122,10 @@ const Sidebar = () => {
           </li>
           <li>
             <Link to="/">
-              <Button className="w-100">
+              <Button
+                className={`w-100 ${activeTab === 5 ? "active" : ""}`}
+                onClick={() => isOpenSubmenu(5)}
+              >
                 <span className="icon">
                   <IoIosSettings />
                 </span>
@@ -90,6 +137,15 @@ const Sidebar = () => {
             </Link>
           </li>
         </ul>
+
+        <br />
+        <div className="logoutWrapper">
+          <div className="logoutBox">
+            <Button variant="contained">
+              <IoMdLogOut /> LogOut
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
