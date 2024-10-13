248945d8d65c97c38d2b46f63691a7648f9f97fb
adiaparmar <adiaparmar@gmail.com>
2024-10-13 23:16:24 +0530

    Dashboard Complete

--- admin/src/pages/Dashboard/components/dashboardBox.js
+++ admin/src/pages/Dashboard/components/dashboardBox.js
@@ -2,7 +2,21 @@ import { HiDotsVertical } from "react-icons/hi";
 import Button from "@mui/material/Button";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
 import TrendingdownIcon from "@mui/icons-material/TrendingDown";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { IoIosTimer } from "react-icons/io";
+import { useState } from "react";
 const DashboardBox = (props) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const ITEM_HEIGHT = 48;
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <div
       className="dashboardBox"
@@ -36,9 +50,41 @@ const DashboardBox = (props) => {
 
       <div className="d-flex align-items-center w-100 bottomEle">
         <h6 className="text-white mb-0 mt-0">Last Month</h6>
-        <Button className="ml-auto toggleIcons">
-          <HiDotsVertical />
-        </Button>
+        <div className="ml-auto">
+          <Button className="ml-auto toggleIcons" onClick={handleClick}>
+            <HiDotsVertical />
+          </Button>
+          <Menu
+            className="dropdown_menu"
+            MenuListProps={{
+              "aria-labelledby": "long-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            slotProps={{
+              paper: {
+                style: {
+                  maxHeight: ITEM_HEIGHT * 4.5,
+                  width: "20ch",
+                },
+              },
+            }}
+          >
+            <MenuItem onClick={handleClose}>
+              <IoIosTimer /> Last Day
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <IoIosTimer /> Last Week
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <IoIosTimer /> Last Month
+            </MenuItem>
+            <MenuItem onClick={handleClose}>
+              <IoIosTimer /> Last Year
+            </MenuItem>
+          </Menu>
+        </div>
       </div>
     </div>
   );
--- admin/src/pages/Dashboard/index.js
+++ admin/src/pages/Dashboard/index.js
@@ -1,10 +1,37 @@
 import { FaUserCircle } from "react-icons/fa";
-import { IoMdCart } from "react-icons/io";
+import { IoIosTimer, IoMdCart } from "react-icons/io";
 import { MdShoppingBag } from "react-icons/md";
 import { GiStarsStack } from "react-icons/gi";
 import DashboardBox from "./components/dashboardBox";
+import { useState } from "react";
+import { Button, Menu, MenuItem } from "@mui/material";
+import { HiDotsVertical } from "react-icons/hi";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Year", "Sales", "Expenses"],
+  ["2013", 1000, 400],
+  ["2014", 1170, 460],
+  ["2015", 660, 1120],
+  ["2016", 1030, 540],
+];
+
+export const options = {
+  backgroundColor: "transparent",
+  chartArea: { width: "100%", height: "80%" },
+};
 
 const Dashboard = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const ITEM_HEIGHT = 48;
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <>
       <div className="right-content w-100">
@@ -31,7 +58,66 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="col-md-4">
-            <div className="box"></div>
+            <div className="box graphBox">
+              <div className="d-flex align-items-center w-100 bottomEle">
+                <h6 className="text-white mb-0 mt-0">Total Sales</h6>
+                <div className="ml-auto">
+                  <Button className="ml-auto toggleIcons" onClick={handleClick}>
+                    <HiDotsVertical />
+                  </Button>
+                  <Menu
+                    className="dropdown_menu"
+                    MenuListProps={{
+                      "aria-labelledby": "long-button",
+                    }}
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    slotProps={{
+                      paper: {
+                        style: {
+                          maxHeight: ITEM_HEIGHT * 4.5,
+                          width: "20ch",
+                        },
+                      },
+                    }}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <IoIosTimer /> Last Day
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IoIosTimer /> Last Week
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IoIosTimer /> Last Month
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <IoIosTimer /> Last Year
+                    </MenuItem>
+                  </Menu>
+                </div>
+              </div>
+
+              <h3 className="text-white font-weight-bold">$3,787,681.00</h3>
+              <p>$3,578.90 in last week</p>
+
+              <Chart
+                chartType="PieChart"
+                width="100%"
+                height="200px"
+                data={data}
+                options={options}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="card shadow border-0 p-3 mt-4">
+          <h3 className="hd">Best Selling Products</h3>
+          <div className="row">
+            <div className="col">
+              <h4>SHOW BY</h4>
+            </div>
           </div>
         </div>
       </div>
